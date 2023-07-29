e4b3870a5b43acbf4abffece17c3aa7997c357c7
abixscodes <harshsingh08608@gmail.com>
2023-07-30 03:32:39 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/react_portfolio-main/",
   server: {
     port: 8001,
   },
